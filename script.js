8f1152a3de7ec0f676d337e68009eb253a7ec402
sajjadahmad30 <sajjadahmad5578@gmail.com>
2024-09-27 11:20:18 +0500

    add click event, and completed class.

--- script.js
+++ script.js
@@ -20,6 +20,11 @@ function addTask(){
         taskList.removeChild(li);
     }
 
+    // add click event to mark as  completed
+    li.onclick=function(){
+        li.classList.toggle("completed")
+    }
+
 
 
     // append the li into  taskList 
