36a004c4316a05be940608689809705699903848
sajjadahmad30 <sajjadahmad5578@gmail.com>
2024-09-27 10:58:38 +0500

    add li and append into tasklist

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function addTask(){
     let taskInput = document.getElementById("taskInput")
     let taskList = document.getElementById("taskList")
-    console.log(taskList)
+
+    // create a new list item
+    let li =document.createElement("li")
+    li.textContent= taskInput.value;
+
+
+    
+    // append the li into task list 
+    taskList.appendChild(li)
+
 }
+
+
