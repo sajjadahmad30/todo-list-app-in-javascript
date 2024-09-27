404b310f59b2d40c73e0945692b6c979cf8c42ea
sajjadahmad30 <sajjadahmad5578@gmail.com>
2024-09-27 11:04:42 +0500

    hadle the empty task

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ function addTask(){
     let taskInput = document.getElementById("taskInput")
     let taskList = document.getElementById("taskList")
 
+    // handle the empty task 
+    if(!taskInput.value.trim()){
+        return alert("Todo can't be empty. Try again")
+    }
+
     // create a new list item
     let li =document.createElement("li")
     li.textContent= taskInput.value;
 
 
-    
-    // append the li into task list 
+
+    // append the li into  taskList 
     taskList.appendChild(li)
 
 }
